e1d6545913c01b5ae4d41d7764a9e41713de7ef0
Vinicius Modena <vmodena94@gmail.com>
2022-07-12 16:16:54 -0300

    feat:handleError finished

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
+import handleAppErrorMiddleware from "./middlewares/handleAppError.middleware";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(handleAppErrorMiddleware);
 
 export default app;
