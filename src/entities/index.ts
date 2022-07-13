cccfe7737c77218f1ac5627f6460956d9843752d
Renata <re_juraski@hotmail.com>
2022-07-13 13:03:29 -0300

    fix/obrigatoryCommit

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,9 +1,11 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm";
 
 @Entity("genres")
 export class Genres {
   @PrimaryGeneratedColumn("uuid")
   id: string;
+
+  //   @OneToMany(() => movieGenres => genre.id)
 
   @Column({ length: 50 })
   name: string;
