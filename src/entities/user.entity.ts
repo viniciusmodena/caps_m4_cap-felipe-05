1a5f4fd553db2869607c8f8e6254c9b9131ef3cc
Andre Luis <andre.luismesquita@hotmail.com>
2022-07-12 21:48:47 -0300

    fix: user entity email proprety set to Unique

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -4,10 +4,12 @@ import {
   Entity,
   OneToMany,
   PrimaryColumn,
+  Unique,
 } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 
 @Entity()
+@Unique("email")
 export class User {
   @PrimaryColumn("uuid")
   user_id: string;
@@ -17,6 +19,9 @@ export class User {
 
   @Column({ length: 50 })
   user_name: string;
+
+  @Column({ length: 50 })
+  email: string;
 
   @Column({ select: false, length: 128 })
   password: string;
