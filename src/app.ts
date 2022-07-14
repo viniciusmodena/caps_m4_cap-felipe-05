894d3ebdd34cfef78c53b826d82068bdb2cf4cb1
Vinicius Modena <vmodena94@gmail.com>
2022-07-14 15:57:56 -0300

    fix: add genreRouter to app

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,7 @@ app.use("/users", userRouter);
 app.use("/sessions", sessionRouter);
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
+app.use("/genres", genreRouter);
 
 app.use(handleAppErrorMiddleware);
 
