f6112391838cb0b0676765e58617555f2ac61d75
Vinicius Modena <vmodena94@gmail.com>
2022-07-18 21:09:16 -0300

    fix: middleware check every possible id params

--- src/middlewares/isUUID.middleware.ts
+++ src/middlewares/isUUID.middleware.ts
@@ -3,11 +3,39 @@ import { Request, Response, NextFunction } from "express";
 import { AppError } from "../errors/appError";
 
 const isUuidMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  const { id } = req.params;
-  const uuidCheck = uuidValidate(id);
+  if (req.params.id) {
+    const uuidCheck = uuidValidate(req.params.id);
+    if (!uuidCheck) {
+      throw new AppError("Id passed in the params must be in uuid format");
+    }
+  }
 
-  if (!uuidCheck) {
-    throw new AppError("Id passed in the params must be in uuid format");
+  if (req.params.user_id) {
+    const uuidCheck = uuidValidate(req.params.user_id);
+    if (!uuidCheck) {
+      throw new AppError("Id passed in the params must be in uuid format");
+    }
+  }
+
+  if (req.params.review_id) {
+    const uuidCheck = uuidValidate(req.params.review_id);
+    if (!uuidCheck) {
+      throw new AppError("Id passed in the params must be in uuid format");
+    }
+  }
+
+  if (req.params.genre_id) {
+    const uuidCheck = uuidValidate(req.params.genre_id);
+    if (!uuidCheck) {
+      throw new AppError("Id passed in the params must be in uuid format");
+    }
+  }
+
+  if (req.params.movie_id) {
+    const uuidCheck = uuidValidate(req.params.movie_id);
+    if (!uuidCheck) {
+      throw new AppError("Id passed in the params must be in uuid format");
+    }
   }
 
   next();
